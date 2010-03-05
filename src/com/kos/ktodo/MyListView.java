0d439626126badc949226441fe9c5b6138d601b9
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2010-03-04 19:27:30 -0500

    scrolling

--- src/com/kos/ktodo/MyListView.java
+++ src/com/kos/ktodo/MyListView.java
@@ -6,10 +6,14 @@ import android.graphics.PixelFormat;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.*;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MyListView extends ListView {
 	private static final String TAG = "MyListView";
@@ -18,19 +22,25 @@ public class MyListView extends ListView {
 	private static final long tapTime = ViewConfiguration.getTapTimeout();
 	private static final long longPressTime = ViewConfiguration.getLongPressTimeout();
 
+	private final int[] xy = new int[2];
+
 	private ImageView mDragView;
 	private WindowManager mWindowManager;
 	private WindowManager.LayoutParams mWindowParams;
-	private int mDragPos;      // which item is being dragged
-	private int mDragItemY;
-	private int mDragStartX, mDragStartY;
-	private int mDragPointY, mDragPointX;    // at what offset inside the item did the user grab it
-	private int mCoordOffsetY, mCoordOffsetX;  // the difference between screen coordinates and coordinates in this view
-//	private Rect mTempRect = new Rect();
 	private Bitmap mDragBitmap;
+	private int dragItemNum;      // which item is being dragged
+	private int lastDragX;
+	//private int mDragItemY;
+	private int mDragStartX, mDragStartY;
+	private int /*mDragPointY,*/ mDragPointX;    // at what offset inside the item did the user grab it
+	private int mCoordOffsetY, mCoordOffsetX;  // the difference between screen coordinates and coordinates in this view
+	private int scaledTouchSlop;
 	private int scaledTouchSlopSquared;
 	private boolean isLongPress = false;
-	private boolean hasNotMoved = true;
+	private boolean hasMoved = false;
+
+	private final ArrayList<MotionEvent> intercepted = new ArrayList<MotionEvent>();
+	private boolean replaying;
 
 	private Handler msgHandler = new Handler() {
 		@Override
@@ -41,67 +51,45 @@ public class MyListView extends ListView {
 		}
 	};
 
-	public MyListView(final Context context) {
-		super(context);
-		final ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
-		scaledTouchSlopSquared = viewConfiguration.getScaledTouchSlop()
-		                         * viewConfiguration.getScaledTouchSlop();
-	}
-
 	public MyListView(final Context context, final AttributeSet attrs) {
 		super(context, attrs);
 		final ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
-		scaledTouchSlopSquared = viewConfiguration.getScaledTouchSlop()
-		                         * viewConfiguration.getScaledTouchSlop();
-	}
-
-	@Override
-	public boolean onInterceptTouchEvent(final MotionEvent ev) {
-		if (ev.getAction() == MotionEvent.ACTION_DOWN) {
-			final int x = (int) ev.getX();
-			final int y = (int) ev.getY();
-			mDragStartX = x;
-			mDragStartY = y;
-			final int itemnum = pointToPosition(x, y);
-			if (itemnum != AdapterView.INVALID_POSITION) {
-				final View item = getChildAt(itemnum - getFirstVisiblePosition());
-				mDragPointY = y - item.getTop();
-				mDragPointX = x - item.getLeft();
-				mCoordOffsetY = ((int) ev.getRawY()) - y;
-				mCoordOffsetX = ((int) ev.getRawX()) - x;
-
-				final int[] xy = new int[2];
-				item.getLocationOnScreen(xy);
-				mDragItemY = xy[1] - mCoordOffsetY / 2;
-				item.setDrawingCacheEnabled(true);
-				// Create a copy of the drawing cache so that it does not get recycled
-				// by the framework when the list tries to clean up memory
-				final Bitmap bitmap = Bitmap.createBitmap(item.getDrawingCache());
-				startDragging(bitmap, x, y);
-				mDragPos = itemnum;
+		scaledTouchSlop = viewConfiguration.getScaledTouchSlop();
+		scaledTouchSlopSquared = scaledTouchSlop * scaledTouchSlop;
+		setOnScrollListener(new OnScrollListener() {
+			public void onScrollStateChanged(final AbsListView view, final int scrollState) {
+				dragView();
 			}
-		}
-		return super.onInterceptTouchEvent(ev);
+
+			public void onScroll(final AbsListView view, final int firstVisibleItem, final int visibleItemCount, final int totalItemCount) {
+				dragView();
+			}
+		});
 	}
 
 	@Override
 	public boolean onTouchEvent(final MotionEvent ev) {
+		if (replaying) return super.onTouchEvent(ev);
 		boolean processed = false;
 		final int action = ev.getAction();
 		switch (action) {
 			case MotionEvent.ACTION_UP:
 			case MotionEvent.ACTION_CANCEL:
-//				final Rect r = mTempRect;
-//				mDragView.getDrawingRect(r);
-				stopDragging();
-				processed = true;
+				if (isDragging()) {
+					stopDragging();
+					processed = true;
+				}
+				if (isPreDragging())
+					stopPreDragging();
 				break;
 
 			case MotionEvent.ACTION_DOWN:
-				// by moving or by canceling the gesture.
+				startPreDragging(ev);
+				//intercepted.add(ev);
 				msgHandler.removeMessages(MSG_LONG_PRESS);
 				msgHandler.sendEmptyMessageAtTime(MSG_LONG_PRESS,
 						ev.getDownTime() + tapTime + longPressTime);
+				isLongPress = false;
 				processed = true;
 				break;
 
@@ -110,42 +98,82 @@ public class MyListView extends ListView {
 					break;
 				final int x = (int) ev.getX();
 				final int y = (int) ev.getY();
-				getChildAt(mDragPos - getFirstVisiblePosition()).setVisibility(View.INVISIBLE);
-				dragView(x, y);
 
-				if (hasNotMoved) {
-					final int deltaXFromDown = (int) (x - mDragStartX);
-					final int deltaYFromDown = (int) (y - mDragStartY);
+				Log.i(TAG, "move: hasMoved=" + hasMoved + ", isDragging:" + isDragging());
+				if (!hasMoved) {
+					final int deltaXFromDown = x - mDragStartX;
+					final int deltaYFromDown = y - mDragStartY;
 					final int distance = (deltaXFromDown * deltaXFromDown)
 					                     + (deltaYFromDown * deltaYFromDown);
-					if (distance > scaledTouchSlopSquared) {
-						hasNotMoved = false;
+					if (distance > scaledTouchSlopSquared)
 						msgHandler.removeMessages(MSG_LONG_PRESS);
+					if (deltaXFromDown > scaledTouchSlop && isPreDragging()) {
+						hasMoved = true;
+						startDragging();
+						processed = true;
 					}
+				} else if (isDragging()) {
+					dragView(x);
+					processed = true;
 				}
-//				if (action == MotionEvent.ACTION_MOVE) {
-//				final View first = getChildAt(mDragPos - getFirstVisiblePosition());
-//				first.setVisibility(View.INVISIBLE);
-//				}
-				processed = true;
 				break;
 		}
 		if (processed) {
-//			Log.i(TAG, "ev: " + ev);
+			Log.i(TAG, "[" + intercepted.size() + "] ev: " + ev);
+			intercepted.add(ev);
 			return true;
+		} else if (intercepted.size() > 0) {
+			replaying = true;
+			for (final MotionEvent event : intercepted) {
+				super.dispatchTouchEvent(event);
+			}
+			replaying = false;
+			//stopPreDragging();
+			intercepted.clear();
 		}
 		return super.onTouchEvent(ev);
 	}
 
-	private void startDragging(final Bitmap bm, final int x, final int y) {
+	private boolean isPreDragging() {
+		return dragItemNum != -1;
+	}
+
+	private boolean isDragging() {
+		return isPreDragging() && mDragView != null;
+	}
+
+	private boolean startPreDragging(final MotionEvent ev) {
 		stopDragging();
 
+		final int x = (int) ev.getX();
+		final int y = (int) ev.getY();
+		final int itemnum = pointToPosition(x, y);
+		if (itemnum == AdapterView.INVALID_POSITION) return false;
+		dragItemNum = itemnum;
+		mDragStartX = x;
+		mDragStartY = y;
+		final View item = getChildAt(itemnum - getFirstVisiblePosition());
+		//mDragPointY = y - item.getTop();
+		mDragPointX = x - item.getLeft();
+		mCoordOffsetY = ((int) ev.getRawY()) - y;
+		mCoordOffsetX = ((int) ev.getRawX()) - x;
+
+		return true;
+	}
+
+	private boolean startDragging() {
+		if (!isPreDragging()) return false;
+
+		final View item = getChildAt(dragItemNum - getFirstVisiblePosition());
+		if (item == null) return false;
+		item.setDrawingCacheEnabled(true);
+		final Bitmap bm = Bitmap.createBitmap(item.getDrawingCache());
 		mWindowParams = new WindowManager.LayoutParams();
 		mWindowParams.gravity = Gravity.TOP;
 		//mWindowParams.x = 0;
-		mWindowParams.x = x - mDragPointX + mCoordOffsetX;
+		mWindowParams.x = mDragStartX - mDragPointX + mCoordOffsetX;
 		//mWindowParams.y = y - mDragPointY + mCoordOffsetY;
-		mWindowParams.y = mDragItemY;
+		mWindowParams.y = getDragItemY();
 
 		mWindowParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
 		mWindowParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
@@ -162,22 +190,50 @@ public class MyListView extends ListView {
 		v.setBackgroundColor(backGroundColor);
 		v.setImageBitmap(bm);
 		v.setVisibility(View.VISIBLE);
+		item.setVisibility(View.INVISIBLE);
 		mDragBitmap = bm;
 
 		mWindowManager = (WindowManager) mContext.getSystemService("window");
 		mWindowManager.addView(v, mWindowParams);
 		mDragView = v;
+		return true;
 	}
 
-	private void dragView(final int x, final int y) {
-		mWindowParams.x = x - mDragPointX + mCoordOffsetX;
-		if (mWindowParams.x < 0)
-			mWindowParams.x = 0;
-		mWindowParams.y = mDragItemY;
-		mWindowManager.updateViewLayout(mDragView, mWindowParams);
+	private int getDragItemY() {
+		final View view = getChildAt(dragItemNum - getFirstVisiblePosition());
+		if (view == null)
+			return -1;
+		view.getLocationOnScreen(xy);
+		return xy[1] - mCoordOffsetY / 2;
+	}
+
+	private void dragView() {
+		dragView(lastDragX);
+	}
+
+	private void dragView(final int x) {
+		if (isDragging()) {
+			mWindowParams.x = x - mDragPointX + mCoordOffsetX;
+			if (mWindowParams.x < 0)
+				mWindowParams.x = 0;
+			final int dragItemY = getDragItemY();
+			if (dragItemY < getTop() || dragItemY + mDragView.getHeight() > getBottom())
+				stopDragging(); //we're out of screen
+			else {
+				mWindowParams.y = dragItemY;
+				mWindowManager.updateViewLayout(mDragView, mWindowParams);
+				lastDragX = x;
+			}
+		}
+	}
+
+	private void stopPreDragging() {
+		dragItemNum = -1;
 	}
 
 	private void stopDragging() {
+		stopPreDragging();
+		intercepted.clear();
 		if (mDragView != null) {
 			final Context mContext = getContext();
 			final WindowManager wm = (WindowManager) mContext.getSystemService("window");
@@ -189,11 +245,11 @@ public class MyListView extends ListView {
 			mDragBitmap.recycle();
 			mDragBitmap = null;
 		}
-		hasNotMoved = true;
-		isLongPress = false;
+		hasMoved = false;
 		msgHandler.removeMessages(MSG_LONG_PRESS);
 		unExpandViews(true);
 	}
+
 
 	private void unExpandViews(final boolean deletion) {
 		for (int i = 0; ; i++) {
@@ -219,8 +275,10 @@ public class MyListView extends ListView {
 	}
 
 	private void handleLongPress() {
+		if (dragItemNum == -1) return;
+		final View view = getChildAt(dragItemNum - getFirstVisiblePosition());
+		if (view == null) return;
 		isLongPress = true;
-		stopDragging();
-		getChildAt(mDragPos - getFirstVisiblePosition()).performLongClick();
+		view.performLongClick();
 	}
 }
