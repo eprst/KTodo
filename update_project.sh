22f66ab90b5475b6e95a278c9e5bf3a1dde30829
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2012-04-11 22:17:55 -0700

    fixes for new SDK changes

--- update_project.sh
+++ update_project.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-../android-sdk-linux_86/tools/android update project --path . --subprojects --name ktodo --target 8 --library ../android-ColorPickerPreference
+#../android-sdk-linux_86/tools/android update project --path . --subprojects --name ktodo --target android-8 --library ../android-ColorPickerPreference
+../android-sdk-linux_86/tools/android update project --path . --subprojects --name ktodo --target android-8
