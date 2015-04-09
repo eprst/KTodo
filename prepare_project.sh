ac5f81891acfdc7bdd9f122654887aeb34bbf8f4
Konstantin Sobolev <konstantin.sobolev@gmail.com>
2015-04-08 20:02:10 -0700

    migrating to gradle

--- prepare_project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# run this once to checkout and prepare required dependencies
-
-pushd .
-cd ..
-git clone https://github.com/eprst/android-ColorPickerPreference.git
-android update project -p .
-popd
--- update_project.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#../android-sdk-linux_86/tools/android update project --path . --subprojects --name ktodo --target android-8 --library ../android-ColorPickerPreference
-#../android-sdk-linux_86/tools/android update project --path . --subprojects --name ktodo --target android-8
-#android update project -p ../android-ColorPickerPreference
-android update project --path . --subprojects --name ktodo --target android-16
